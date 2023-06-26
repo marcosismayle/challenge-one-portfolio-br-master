c8dd247dfb96aace54b948211a631636a756c3a0
marcosismayle <marcosismayle@gmail.com>
2023-06-26 16:54:50 -0300

    Barra de navegação concluída/seção sobre iniciada

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,1 +1,35 @@
 //Seu JavaScript de validação aqui
+
+var iconMenu = document.querySelector('.icon-menu'),
+    menu = document.querySelector('.menu__list'),
+    menuLink = document.querySelectorAll('.menu-link');
+
+iconMenu.addEventListener('click', openMenu);
+
+menuLink.forEach(function(el) {
+  el.addEventListener('click', openSubmenu);
+});
+
+function openMenu() {
+  
+  if(menu.classList.contains('open')) {
+    menu.classList.add('close');
+    iconMenu.classList.remove('icon-closed');
+    
+    setTimeout(function(){ menu.classList.remove('open'); }, 1300);
+    
+  } else {
+    menu.classList.remove('close');
+    menu.classList.add('open');
+    iconMenu.classList.add('icon-closed');
+  } 
+}
+
+function openSubmenu(event) {
+  
+if (event.currentTarget.classList.contains("active")) {
+    event.currentTarget.classList.remove("active");
+  } else {
+    event.currentTarget.classList.add("active");
+  }
+}
